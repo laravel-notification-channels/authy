423bdd52060e0e5df03aa35ccfa3ea565b8124ae
Abdelrahman Omran <me@omranic.com>
2018-05-21 21:45:23 +0200

    Clean file headers

--- tests/MessageTest.php
+++ tests/MessageTest.php
@@ -1,18 +1,6 @@
 <?php
 
 declare(strict_types=1);
-/*
- * NOTICE OF LICENSE
- *
- * Part of the Authy Notification Channel for Laravel Package.
- *
- * This source file is subject to The MIT License (MIT)
- * that is bundled with this package in the LICENSE file.
- *
- * Package: Authy Notification Channel for Laravel Package
- * License: The MIT License (MIT)
- * Link:    https://rinvex.com
- */
 
 namespace NotificationChannels\Authy\Test;
 
